1785cad97dc1007dbf1256a05c8b531c8150b3b7
Dan Chianucci <dan.chianucci@gmail.com>
2020-06-29 09:19:59 -0400

    Fixed linting

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,10 +1,18 @@
 import pytest
 
 
+def setup_function(function):
+    print("setting up", function)
+
+
 def test_import():
     from sphinxcontrib import vhdldomain
     print(vhdldomain)
 
+
+def test_something():
+    from sphinxcontrib import vhdldomain  # noqa F401
+    print("idk")
 
 
 if __name__ == '__main__':
